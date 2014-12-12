46c8d42cb03f20e7af301070dc37816b1af2d38e
Tingjun Yang <tjyang@argoneutgpvm03.fnal.gov>
2014-12-12 11:22:09 -0600

    put back the argoneut list temprarily

--- Filters/ChannelFilter.cxx
+++ Filters/ChannelFilter.cxx
@@ -29,6 +29,32 @@ filter::ChannelFilter::ChannelFilter()
   
   // populate the set of bad channels for this detector
   // This code should eventually hook up to a database
+  // put argoneut list back in temporarily, this will be removed when a service is implemented
+  if(geochan->DetectorName() == "argoneut"){
+    fBadChannels.insert(22);
+    fBadChannels.insert(65);
+    fBadChannels.insert(237);
+    fBadChannels.insert(307);
+    fBadChannels.insert(308);
+    fBadChannels.insert(309);
+    fBadChannels.insert(310);
+    fBadChannels.insert(311);
+    fBadChannels.insert(410);
+    fBadChannels.insert(412);
+    fBadChannels.insert(438);
+    fBadChannels.insert(439);
+    fBadChannels.insert(448);
+    
+
+    fNoisyChannels.insert(31);
+    fNoisyChannels.insert(41);
+    fNoisyChannels.insert(108);
+    fNoisyChannels.insert(120);
+    fNoisyChannels.insert(121);
+    fNoisyChannels.insert(124);
+    fNoisyChannels.insert(392);
+    fNoisyChannels.insert(399);
+  }
   
 }
 
