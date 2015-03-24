be25e9921eaab6dfdca59b5c6b9f372a77dc28bc
Brandon Eberly <eberly@fnal.gov>
2015-03-24 17:50:35 -0500

    minor cleanup

--- CalibrationDBI/WebDBI/WebDBIConstants.h
+++ CalibrationDBI/WebDBI/WebDBIConstants.h
@@ -3,10 +3,6 @@
 
 #include <string>
 namespace lariov{
-  
-  const std::string  kDEFAULT_WDA_CONN_STRING = "default";
-  const unsigned int kDEFAULT_WDA_TIMEOUT_SEC = 5;
-  
   const unsigned int kNUMBER_HEADER_ROWS = 4;
   const unsigned int kBUFFER_SIZE = 40;
 }
--- CalibrationDBI/WebDBI/WebError.cxx
+++
@@ -1,6 +0,0 @@
-#ifndef WEBDB_WEBERROR_CXX
-#define WEBDB_WEBERROR_CXX
-
-#include "WebError.h"
-
-#endif
