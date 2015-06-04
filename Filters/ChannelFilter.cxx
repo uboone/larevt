6545d63ba07042745a7927a26d6dcdfcc811f9c2
Tracy Usher <usher@slac.stanford.edu>
2015-06-04 13:59:59 -0700

    Updating the current ChannelFilter implementation for use in eliminating the non-physical channels read out in the MicroBooNE noise data. The original concept is now extended to assign a status, from an enumerated list, to a channel with current values: GOOD, NOISY, DEAD and NOTPHYSICAL. The object is also modified to include the current list of known noisy wires from David (via Leon) and to suppress spurious messages. It is believed this object will eventually become a larsoft service with access to a database...

--- Filters/ChannelFilter.cxx
+++ Filters/ChannelFilter.cxx
@@ -22,10 +22,6 @@
 filter::ChannelFilter::ChannelFilter()
 {
   art::ServiceHandle<geo::Geometry> geochan;
-
-  LOG_WARNING("ChannelFilter") << "ChannelFilter is not defined for any experiment. "
-			       << "\n Each experiment should implement its own filter for "
-			       << "bad channels in its experimental code base";
   
   // populate the set of bad channels for this detector
   // This code should eventually hook up to a database
@@ -55,6 +51,27 @@ filter::ChannelFilter::ChannelFilter()
     fNoisyChannels.insert(392);
     fNoisyChannels.insert(399);
   }
+  else if(geochan->DetectorName().find("microboone")==0)
+  {
+      const size_t nNoisy = 81;
+      size_t noisyChan[nNoisy] = {
+            2351, 2350, 2349, 2348, 2347, 2346, 2345, 2344, 2343, 2342, 2341, 2340, 2339, 2338, 2337
+          , 623 , 622 , 621 , 620 , 619 , 618 , 617 , 611 , 628 , 627 , 626 , 625 , 624 , 648 , 647 , 646 , 645 , 644 , 643 , 642
+          , 641 , 640 , 671 , 670 , 669 , 666 , 663 , 662 , 661 , 660 , 659 , 658 , 657 , 656 , 303 , 302 , 301 , 300 , 319 , 307
+          , 306 , 305 , 304 , 399 , 384 , 400 , 5328, 5327, 2992, 15  , 14  , 13  , 12  , 11  , 10  , 9   , 8   , 7   , 6   , 3078
+          , 5   , 4   , 3   , 2   , 1   , 0
+      };
+      
+      for (size_t idx = 0; idx < nNoisy; idx++) fChannelStatusMap[noisyChan[idx]] = NOISY;
+      
+      LOG_DEBUG("ChannelFilter") << fChannelStatusMap.size() << " wires with status other than GOOD for MicroBooNE";
+  }
+  else
+      // Issue the following warning only if not argoneut or microboone
+      // (sorry Gianluca, I know this module should/will be replaced so we don't need to see the message!)
+        LOG_WARNING("ChannelFilter") << "ChannelFilter is not defined for any experiment. "
+			                         << "\n Each experiment should implement its own filter for "
+			                         << "bad channels in its experimental code base";
   
 }
 
@@ -76,5 +93,24 @@ bool filter::ChannelFilter::NoisyChannel(uint32_t channel)
   if(fNoisyChannels.find(channel) != fNoisyChannels.end()) return true;
   return false;
 }
+
+///////////////////////////////////////////////////////
+filter::ChannelFilter::ChannelStatus filter::ChannelFilter::GetChannelStatus(uint32_t channel) const
+{
+    // In theory ChannelFilter should be a service in which case one can
+    // keep a data member reference for the geometry service... which can
+    // be updated as needed.
+    // Until then we need to recover each time we call this routine... painful!
+    art::ServiceHandle<geo::Geometry> geochan;
+    
+    // Make sure the channel is in range!
+    if (channel > geochan->Nchannels()) return ChannelFilter::NOTPHYSICAL;
+    
+    if (fChannelStatusMap.find(channel) != fChannelStatusMap.end())
+        return fChannelStatusMap.find(channel)->second;
+    
+    return ChannelFilter::GOOD;
+}
+
 
 
