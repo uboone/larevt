b67db3f076fbd18508f16c8bf75bbd9f73ea7ce2
Brandon Eberly <eberly@fnal.gov>
2016-12-02 18:16:37 -0600

    Added support for retrieving boolean data from database, and storing booleans in CalibrationExtraInfo

--- larevt/CalibrationDBI/IOVData/CalibrationExtraInfo.h
+++ larevt/CalibrationDBI/IOVData/CalibrationExtraInfo.h
@@ -20,12 +20,14 @@ namespace lariov {
       std::string const& GetName() const
       { return fName; }
       
+      void AddOrReplaceBoolData(std::string const& label, bool const data);
       void AddOrReplaceIntData(std::string const& label, int const data);
       void AddOrReplaceVecIntData(std::string const& label, std::vector<int> const& data);
       void AddOrReplaceFloatData(std::string const& label, float const data);
       void AddOrReplaceVecFloatData(std::string const& label, std::vector<float> const& data);
       void AddOrReplaceStringData(std::string const& label, std::string const& data);
       
+      bool GetBoolData(std::string const& label) const;
       int GetIntData(std::string const& label) const;
       std::vector<int> const& GetVecIntData(std::string const& label) const;
       float GetFloatData(std::string const& label) const;
@@ -40,6 +42,8 @@ namespace lariov {
     private:
     
       std::string fName;
+      
+      std::map<std::string, bool> fBoolData;
       
       std::map<std::string, int> fIntData;
       std::map<std::string, std::vector<int> > fVecIntData;
